e55cb640e878c9ddaf258d113c652c1cd62dac0d
nealwp <mrprestonneal@gmail.com>
2025-06-22 14:58:18 -0700

    tests work

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,10 +1,9 @@
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
     @Test
-    void addition() {
+    public void addition() {
         assertEquals(2, 1 + 1);
     }
 }
