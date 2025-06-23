8d50370ff0a32555e3c974ce4df3bff014fc87db
nealwp <mrprestonneal@gmail.com>
2025-06-22 17:15:23 -0700

    collecing stress rating

--- src/main/java/MentalHealthTracker.java
+++ src/main/java/MentalHealthTracker.java
@@ -1,18 +1,32 @@
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class MentalHealthTracker {
     public int mood;
+    public int stress;
+    private PrintStream printer;
+    private Scanner scanner;
+
+    public MentalHealthTracker(PrintStream printer, Scanner scanner) {
+        this.printer = printer;
+        this.scanner = scanner;
+    }
 
     public String sayHello() {
         return "hello, it's time for your check-in :)";
     }
 
     public void askUserForMoodRating() {
-        Scanner input = new Scanner(System.in);
         while (this.mood < 1 || this.mood > 10) {
-            System.out.print("On a scale of 1 to 10, please rate your mood: ");
-            this.mood = input.nextInt();
+            this.printer.print("On a scale of 1 to 10, please rate your mood: ");
+            this.mood = this.scanner.nextInt();
         }
-        input.close();
+    }
+
+    public void askUserForStressRating() {
+        while (this.stress < 1 || this.stress > 10) {
+            this.printer.print("On a scale of 1 to 10, please rate your stress: ");
+            this.stress = this.scanner.nextInt();
+        }
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,33 +1,51 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
+import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
 
-    private final MentalHealthTracker tracker = new MentalHealthTracker();
-
     @Test
     public void tracker_shouldGreetTheUser() {
+        MentalHealthTracker tracker = new MentalHealthTracker(System.out, new Scanner(System.in));
         assertEquals("hello, it's time for your check-in :)", tracker.sayHello());
     }
 
     @Test
     void tracker_shouldCollectMoodRatingFromUser() {
-        String simulatedInput = "5";
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(simulatedInput.getBytes());
-        System.setIn(inputStream);
+        String moodRatingInput = "5";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(moodRatingInput.getBytes());
+        MentalHealthTracker tracker = new MentalHealthTracker(System.out, new Scanner(inputStream));
         tracker.askUserForMoodRating();
         assertEquals(5, tracker.mood);
     }
 
     @Test
     void tracker_shouldWaitForValidMoodRating() {
-        String simulatedInput = "11\n-3\n6"; // first two inputs are invalid, third is valid
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(simulatedInput.getBytes());
-        System.setIn(inputStream);
+        String moodRatingInput = "11\n-3\n6"; // first two inputs are invalid, third is valid
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(moodRatingInput.getBytes());
+        MentalHealthTracker tracker = new MentalHealthTracker(System.out, new Scanner(inputStream));
         tracker.askUserForMoodRating();
         assertEquals(6, tracker.mood);
+    }
+
+    @Test
+    void tracker_shouldCollectStressRatingFromUser() {
+        String stressRatingInput = "7";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(stressRatingInput.getBytes());
+        MentalHealthTracker tracker = new MentalHealthTracker(System.out, new Scanner(inputStream));
+        tracker.askUserForStressRating();
+        assertEquals(7, tracker.stress);
+    }
+
+    @Test
+    void tracker_shouldWaitForValidStressRating() {
+        String stressRatingInput = "11\n-3\n7"; // first two inputs are invalid, third is valid
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(stressRatingInput.getBytes());
+        MentalHealthTracker tracker = new MentalHealthTracker(System.out, new Scanner(inputStream));
+        tracker.askUserForStressRating();
+        assertEquals(7, tracker.stress);
     }
 }
