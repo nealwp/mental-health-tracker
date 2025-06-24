c41b431c27daf98637952883ae531ed7a495436f
nealwp <mrprestonneal@gmail.com>
2025-06-23 20:09:15 -0700

    reads back entries from file

--- src/main/java/MentalHealthTracker.java
+++ src/main/java/MentalHealthTracker.java
@@ -1,5 +1,12 @@
 import java.io.PrintStream;
 import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDate;
+import java.nio.file.Path;
+import java.util.List;
 
 public class MentalHealthTracker {
     public int mood;
@@ -7,6 +14,7 @@ public class MentalHealthTracker {
     public String reflection;
     private PrintStream printer;
     private Scanner scanner;
+    private Path path = new File("output.csv").toPath();
 
     public MentalHealthTracker(PrintStream printer, Scanner scanner) {
         this.printer = printer;
@@ -18,10 +26,21 @@ public class MentalHealthTracker {
         askUserForMoodRating();
         askUserForStressRating();
         askUserForReflection();
+        saveEntry();
+        boolean userWantsToSeePastEntries = askUserIfTheyWantToSeePastEntries();
+        if (userWantsToSeePastEntries) {
+            showPastEntries();
+        } else {
+            sayGoodbye();
+        }
     }
 
     public void sayHello() {
         printer.println("hello, it's time for your check-in :)");
+    }
+
+    public void sayGoodbye() {
+        printer.println("goodbye.");
     }
 
     public void askUserForMoodRating() {
@@ -51,4 +70,40 @@ public class MentalHealthTracker {
 
     }
 
+    public void saveEntry() {
+        String today = LocalDate.now().toString();
+        String row = String.format("%s, %d,%d,%s", today, mood, stress, reflection);
+
+        try {
+            Files.write(path, List.of(row), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            printer.println("Your entry has been saved. Thanks for checking in!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean askUserIfTheyWantToSeePastEntries() {
+        String response = "";
+        while (!response.equalsIgnoreCase("n") && !response.equalsIgnoreCase("y")) {
+            printer.print("Would you like to see past entries? (y/n): ");
+            response = scanner.next();
+        }
+        return response.equalsIgnoreCase("y");
+    }
+
+    public void showPastEntries() {
+        try {
+            List<String> lines = Files.readAllLines(path);
+
+            for (String line : lines) {
+                String[] fields = line.split(",", -1);
+
+                System.out.printf("Date: %s; Mood: %d; Stress: %d; Reflection: %s%n",
+                        fields[0], fields[1], fields[2], fields[3]);
+            }
+
+        } catch (IOException e) {
+            System.err.println("Failed to read file: " + e.getMessage());
+        }
+    }
 }
